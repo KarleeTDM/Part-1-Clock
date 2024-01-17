15edc1f133bbc75bc0181f31c1b337a02224df91
mackkarl <mackkarl@OA-S141-10>
2024-01-17 14:55:20 -0500

    Meep moop. Please commit and push.

---
+++ Assets/Week 1/Scripts/Missle.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Missle : MonoBehaviour
+{
+    public float speed = 10f;
+    Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = new Vector2(speed * Time.deltaTime, 0);
+        rigidbody.MovePosition(rigidbody.position + direction);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("missle collision");
+        Destroy(gameObject);
+    }
+}
