06301c02aa75ce6359831ea65bce1a0de3c5982b
mackkarl <mackkarl@OA-S141-10>
2024-01-17 15:57:14 -0500

    I AM UPSET.

---
+++ Assets/Week 2/Scripts/Camera.cs
@@ -0,0 +1,14 @@
+using Cinemachine;
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public 
+    }
+}
